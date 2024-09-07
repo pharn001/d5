ca962ebc58056659d844909b853d035b83611b3c
sompharn <150797864+pharn001@users.noreply.github.com>
2024-09-07 07:57:10 +0700

    Add files via upload

---
+++ policy.tsx
@@ -0,0 +1,10 @@
+import { StringAdapter } from "casbin";
+
+export const adapter = new StringAdapter(`
+p, admin, user, (list)|(create)
+p, admin, user/*, (edit)|(show)|(delete)
+p, admin, log-out, (create)
+p, admin, profile/*, (edit)|(show)|(delete)
+p, admin, profile, (list)|(create)
+
+`);
